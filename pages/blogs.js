3f833759542fa8d00e478911f4aaac48d6c899bd
AkashRajiwale <rajiwaleakash777@gmail.com>
2021-12-07 22:16:56 +0530

    Created Simple Structure

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,21 +1,24 @@
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head"
 
-export default function Home() {
+const blogs = () => {
   return (
     <div className="flex items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Favy</title>
+        <title>Favlent - Blogs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-500" href="https://nextjs.org">
-            favy!
+          Welcome to{" "}
+          <a className="text-blue-500" >
+            Blog Page!
           </a>
         </h1>
-       </main>
+      </main>
     </div>
-  )
-}
+  );
+};
+
+export default blogs;
---
+++ pages/design.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Head from 'next/head'
+const design = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen py-2">
+            <Head>
+        <title>Favlent -  Dashboard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{" "}
+          <a className="text-blue-500">
+            Design Page!
+          </a>
+        </h1>
+      </main>
+        </div>
+    )
+}
+
+export default design
