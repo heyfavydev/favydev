a5f7a9a942eb68af698869352929bce3f30c4620
git-yj <1024byte.code@gmail.com>
2021-12-08 11:13:21 +0530

    Navbar Design Tweak to brand

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,12 +3,12 @@ import Link from 'next/link'
 const Navbar = () => {
     return (
         <div>
-            <div className="flex h-12 bg-gray-900 items-center">
-                <h1 className="text-white text-2xl ml-5"><Link href="/"> Favy </Link></h1>
-                <ul className="flex ml-5 text-2x1 text-white">
-                    <li className="my-2 p-4 hover:text-gray-400 transition ease duration-300"><Link href="/dashboard">Dashboard</Link></li>
-                    <li className="my-2 p-4 hover:text-gray-400 transition ease duration-300"><Link href="/blogs">Blogs</Link></li>
-                    <li className="my-2 p-4 hover:text-gray-400 transition ease duration-300"><Link href="/design">Design</Link></li>
+            <div className="flex h-11 bg-white items-center text border-b border-gray-300">
+                <h1 className="text-gray-700 text-xl ml-5 antialiased font-semibold"><Link href="/"> Favy </Link></h1>
+                <ul className="flex ml-3 text-base text-gray-500 antialiased font-medium">
+                    <li className="my-2 p-2 hover:text-gray-900 transition ease duration-300"><Link href="/dashboard">Dashboard</Link></li>
+                    <li className="my-2 p-2 hover:text-gray-900 transition ease duration-300"><Link href="/blogs">Blogs</Link></li>
+                    <li className="my-2 p-2 hover:text-gray-900 transition ease duration-300"><Link href="/design">Design</Link></li>
                 </ul>
             </div>
         </div>
