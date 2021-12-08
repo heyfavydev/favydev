e0f6f469a77340f994b64ada4ee23660063797da
git-yj <1024byte.code@gmail.com>
2021-12-08 10:50:35 +0530

    favelent to favy

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <div>
             <div className="flex h-12 bg-gray-900 items-center">
-                <h1 className="text-white text-3xl ml-5"><Link href="/"> Favlent </Link></h1>
+                <h1 className="text-white text-2xl ml-5"><Link href="/"> Favy </Link></h1>
                 <ul className="flex ml-5 text-2x1 text-white">
                     <li className="my-2 p-4 hover:text-gray-400 transition ease duration-300"><Link href="/dashboard">Dashboard</Link></li>
                     <li className="my-2 p-4 hover:text-gray-400 transition ease duration-300"><Link href="/blogs">Blogs</Link></li>
