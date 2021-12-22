441a2f7f0e629f98647cfc5b4ff84fec944c7e62
AkashRajiwale <rajiwaleakash777@gmail.com>
2021-12-22 18:45:55 +0530

    error

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -5,14 +5,22 @@ import { getAuth } from "firebase/auth";
 
 // Initialize Firebase
 const clientCredentials = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyC0sFxTli2eQpwPMDiuoohKhNi8eXzRZEg",
+  authDomain: "favydev-93dc3.firebaseapp.com",
+  databaseURL: "https://favydev-93dc3-default-rtdb.firebaseio.com",
+  projectId: "favydev-93dc3",
+  storageBucket: "favydev-93dc3.appspot.com",
+  messagingSenderId: "422419216535",
+  appId: "1:422419216535:web:2576056fe03f89104c51d2"
 };
+
+// apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 
 const firebaseApp = initializeApp(clientCredentials);
 const auth = getAuth(firebaseApp);
